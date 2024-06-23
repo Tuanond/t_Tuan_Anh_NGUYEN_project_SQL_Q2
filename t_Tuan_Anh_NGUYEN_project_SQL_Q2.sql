dd57bc2f0f2cc0b674925e3027645814a438f771
Tuan Anh Nguyen <ondra.nguyen@gmail.com>
2024-06-23 11:20:27 +0200

    UNION sum mleko+chleba 2006 a 2018

--- t_Tuan_Anh_NGUYEN_project_SQL_Q2.sql
+++ t_Tuan_Anh_NGUYEN_project_SQL_Q2.sql
@@ -28,18 +28,76 @@ ORDER BY cp.category_code, cpay.payroll_year;
 
 
 
-
-
-
-WITH prices_chleba_mleko AS (
-SELECT *
+SELECT
+	cpay.value AS payroll_value,
+	cpay.payroll_year,
+	cp.category_code,
+	cpc.name,
+	cp.value,
+	cpc.price_value,
+	cpc.price_unit 
 FROM czechia_price AS cp
 LEFT JOIN czechia_price_category AS cpc 
 	ON cp.category_code = cpc.code
-WHERE cp.category_code IN ('114201', '111301')
-ORDER BY cp.category_code, cp.date_from)
-SELECT 
-FROM czechia_payroll AS cpay
-LEFT JOIN prices_chleba_mleko AS pcm
-	ON cpay.payroll_year = year(pcm.date_from);
+LEFT JOIN czechia_payroll AS cpay
+	ON YEAR(cp.date_from) = cpay.payroll_year
+WHERE cp.category_code = '114201' AND cpay.value_type_code = 5958 AND payroll_year = 2006; -- mléko
 
+
+
+-- sum of chleba 2006 and 2018
+SELECT 
+	cp.category_code,
+	cpc.name,
+	YEAR(cp.date_from),
+	sum(cp.value) sum_of_avg_prices,
+	sum(cpc.price_value) AS sum_of_kg,
+	cpc.price_unit
+FROM czechia_price AS cp
+LEFT JOIN czechia_price_category AS cpc 
+	ON cp.category_code = cpc.code
+WHERE cp.category_code = '111301' AND YEAR(cp.date_from) IN ('2006', '2018')
+GROUP BY YEAR(cp.date_from);
+
+-- sum of mleko 2006 and 2018
+
+SELECT 
+	cp.category_code,
+	cpc.name,
+	YEAR(cp.date_from),
+	sum(cp.value) sum_of_avg_prices,
+	sum(cpc.price_value) AS sum_of_liters, 	
+	cpc.price_unit
+FROM czechia_price AS cp
+LEFT JOIN czechia_price_category AS cpc 
+	ON cp.category_code = cpc.code
+WHERE cp.category_code = '114201' AND YEAR(cp.date_from) IN ('2006', '2018')
+GROUP BY YEAR(cp.date_from);
+
+
+-- union of mleko a chleba v roce 2006 a 2018
+SELECT 
+	cp.category_code,
+	cpc.name,
+	YEAR(cp.date_from),
+	sum(cp.value) sum_of_avg_prices,
+	sum(cpc.price_value) AS sum_of_kg,
+	cpc.price_unit
+FROM czechia_price AS cp
+LEFT JOIN czechia_price_category AS cpc 
+	ON cp.category_code = cpc.code
+WHERE cp.category_code = '111301' AND YEAR(cp.date_from) IN ('2006', '2018')
+GROUP BY YEAR(cp.date_from)
+UNION
+SELECT 
+	cp.category_code,
+	cpc.name,
+	YEAR(cp.date_from),
+	sum(cp.value) sum_of_avg_prices,
+	sum(cpc.price_value) AS sum_of_liters, 	
+	cpc.price_unit
+FROM czechia_price AS cp
+LEFT JOIN czechia_price_category AS cpc 
+	ON cp.category_code = cpc.code
+WHERE cp.category_code = '114201' AND YEAR(cp.date_from) IN ('2006', '2018')
+GROUP BY YEAR(cp.date_from);
