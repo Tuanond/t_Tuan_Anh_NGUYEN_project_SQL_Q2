8c27d1dad6ed7004ae1f0dbff04bbab23f39bf69
Tuan Anh Nguyen <ondra.nguyen@gmail.com>
2024-06-23 10:34:06 +0200

    prices+payroll

--- t_Tuan_Anh_NGUYEN_project_SQL_Q2.sql
+++ t_Tuan_Anh_NGUYEN_project_SQL_Q2.sql
@@ -3,13 +3,43 @@
 
 
 SELECT *
-FROM czechia_payroll AS cp
-WHERE value_type_code = 5958;
+FROM czechia_payroll AS cpay
+WHERE value_type_code = 5958);
 
 
 -- 114201 = mléko, 111301 = chleba
+
+
+SELECT
+	cpay.value AS payroll_value,
+	cpay.payroll_year,
+	cp.category_code,
+	cpc.name,
+	cp.value,
+	cpc.price_value,
+	cpc.price_unit 
+FROM czechia_price AS cp
+LEFT JOIN czechia_price_category AS cpc 
+	ON cp.category_code = cpc.code
+LEFT JOIN czechia_payroll AS cpay
+	ON YEAR(cp.date_from) = cpay.payroll_year
+WHERE cp.category_code IN ('114201', '111301') AND cpay.value_type_code = 5958
+ORDER BY cp.category_code, cpay.payroll_year;
+
+
+
+
+
+
+WITH prices_chleba_mleko AS (
 SELECT *
 FROM czechia_price AS cp
 LEFT JOIN czechia_price_category AS cpc 
 	ON cp.category_code = cpc.code
-WHERE cp.category_code IN ('114201', '111301');
+WHERE cp.category_code IN ('114201', '111301')
+ORDER BY cp.category_code, cp.date_from)
+SELECT 
+FROM czechia_payroll AS cpay
+LEFT JOIN prices_chleba_mleko AS pcm
+	ON cpay.payroll_year = year(pcm.date_from);
+
